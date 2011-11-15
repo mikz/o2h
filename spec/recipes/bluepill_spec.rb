a3432f5a36e3ea49a879f689dd90036df327f13f
Michal Cichra <michal@o2h.cz>
2011-11-15 14:05:19 +0100

    really integrate with capistrano-spec

--- spec/recipes/bluepill_spec.rb
+++ spec/recipes/bluepill_spec.rb
@@ -7,6 +7,14 @@ describe "bluepill" do
     subject.find_task("bluepill:start").should_not be_nil
   end
 
+  it "performs bluepill:stop before deploy" do
+    subject.should callback('bluepill:stop').before('deploy')
+  end
+
+  it "performs bluepill:start after deploy" do
+    subject.should callback('bluepill:start').after('deploy')
+  end
+
   it "should set :bluepill" do
     subject[:bluepill].should_not be_nil
   end
--- spec/support/shared_contexts/capistrano.rb
+++ spec/support/shared_contexts/capistrano.rb
@@ -2,12 +2,15 @@ require 'capistrano'
 require 'capistrano-spec'
 
 shared_context :capistrano do
+  include Capistrano::Spec::Matchers
+
   let(:capistrano)  { Capistrano::Configuration.new }
   let(:recipe_name) { self.class.top_level_description }
   let(:recipe_path) { recipe(recipe_name) }
   subject         { capistrano }
 
   before do
+    capistrano.extend Capistrano::Spec::ConfigurationExtension
     capistrano.load recipe_path
   end
 
