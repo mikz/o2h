15ad4d63a05ee9683cdb4f9d0e0e49973e3dc272
Michal Cichra <michal@o2h.cz>
2011-12-08 15:26:54 +0100

    unicorn deploy recipe

--- lib/o2h/recipes/host/unicorn.rb
+++ lib/o2h/recipes/host/unicorn.rb
@@ -1,16 +1,20 @@
+set(:unicorn_binary){ "bundle exec unicorn" }
+set(:unicorn_config){ "config/unicorn.rb" }
+set(:unicorn_pid){ "#{shared_path}/pids/unicorn.pid" }
+
 namespace :deploy do
   desc "Zero-downtime restart of Unicorn"
   task :restart, :except => { :no_release => true } do
-    run "kill -s USR2 `cat /tmp/unicorn.my_site.pid`"
+    run "kill -s USR2 `cat #{unicorn_pid}`"
   end
 
   desc "Start unicorn"
   task :start, :except => { :no_release => true } do
-    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+    run "cd #{current_path}; #{unicorn_binary} -E #{rails_env} -c #{unicorn_config} -D"
   end
 
   desc "Stop unicorn"
   task :stop, :except => { :no_release => true } do
-    run "kill -s QUIT `cat /tmp/unicorn.my_site.pid`"
+    run "kill -s QUIT `cat #{unicorn_pid}`"
   end
 end
