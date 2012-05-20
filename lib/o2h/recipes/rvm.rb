60e851fc82678cb1ac42f43753e6af8d3c68cdeb
Michal Cichra <michal@o2h.cz>
2012-05-20 22:15:58 +0200

    remove rvm integration

--- lib/o2h/recipes/rvm.rb
+++
@@ -1,6 +0,0 @@
-require "rvm/capistrano"
-
-set(:rvm_ruby, "ree")
-set(:rvm_type, :system)
-set(:rvm_gemset) { fetch(:application) }
-set(:rvm_ruby_string) { [fetch(:rvm_ruby), fetch(:rvm_gemset)].compact.join('@') }
